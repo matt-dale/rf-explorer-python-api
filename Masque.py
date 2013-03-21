93101e32e6fe05881d1af8e32cdc061ec51660c2
Matt_Dale <matthewdale@NJ-DALETOSHIBA.MSQSND.com>
2013-03-21 13:31:52 -0400

    added some quasi-error correction

--- Masque.py
+++ Masque.py
@@ -1,13 +1,13 @@
-from RFExplorer import *
+#!/usr/bin/python
 
-RFE = RFExplorer(5)
+from RFExplorer import *
 
 def first_chunk(RFE):
     """
     Creates the compiled dictionary for the band from 450-512 MHz in 1.5MHz chunks
     Takes 2 minutes to get all the data
     
-    returns compiled dictionary to feed into the CSV creator
+    Returns compiled dictionary to feed into the CSV creator
     
     """
     start_freq = 450000
@@ -16,13 +16,14 @@ def first_chunk(RFE):
     freq_list = ['450000']
     for i in range(chunks):
         freq_list.append(int(freq_list[i])+1550)
-    end_time = time.time() + 120
+    end_time = time.time() + 900
+    #let's stop  this first chunk after 15 minutes
     first_chunk = {}
     while time.time() < end_time:
         for i in range(chunks):
-            print i
             start = freq_list[i]
             end = str(int(start)+1550)
+            print 'sweeping from %s to %s' % (start, end)
             dict = RFE.quick_sweep(start,end)
             for i, v in dict.iteritems():
                 if i in first_chunk:
@@ -48,10 +49,12 @@ def second_chunk(RFE):
         freq_list.append(int(freq_list[i])+3100)
     end_time = time.time() + 120
     second_chunk = {}
+    #for each chunk, sit for 30 secs
     while time.time() < end_time:
         for i in range(chunks):
             start = freq_list[i]
             end = str(int(start)+3100)
+            print 'sweeping from %s to %s' % (start, end)
             dict = RFE.quick_sweep(start,end)
             for i, v in dict.iteritems():
                 if i in second_chunk:
@@ -61,16 +64,27 @@ def second_chunk(RFE):
                     second_chunk[i] = v
     return second_chunk
     
-def masque_RF_data(RFE, filename):
+def masque_RF_data():
+    print "Here's a list of your serial ports."
+    x = list_serial_ports()
+    print x
+    print "Do you know which number the RFE is?"
+    print """
+            If you don't know...remember the above list of ports, 
+            then unplug the RFE and run the program again to determine by which number went away from the above list.
+          """
+    num = raw_input("Which serial port number is the RFE? Only enter the number, not the word 'COM'.>>> ")
+    RFE = RFExplorer(int(num))
+    filename = raw_input("Enter the name of the CSV file that you want generated.>>> ")
+    print 'Sweeping from 450-512...'
+    print 'Please wait for the program to exit...'
     d1 = first_chunk(RFE)
+    print 'Sweeping from 512-698...'
     d2 = second_chunk(RFE)
     final_dict = dict(d1, **d2)
     success = RFE.make_csv(filename, final_dict)
     return True
     
-            
-            
-        
-        
     
-    
+if __name__ == '__main__':
+    masque_RF_data()
--- RFExplorer.py
+++ RFExplorer.py
@@ -150,6 +150,7 @@ class RFExplorer:
         Raises:
             SerialException:None
             Used to break a while loop if all has gone to hell
+            ValueError: If the conversion of datapoints fails for a specific 
         """
         first = self.ser.readline()
         if not first.startswith('$S'):
@@ -168,16 +169,29 @@ class RFExplorer:
         result = "".join(result.splitlines())
         separated = result.split('=')
         final_results = []
-        for i in separated:
-            if (len(i) == 0) or i.startswith("$"):
-                continue
-            i = (int(i, 16)/2)*-1#convert to dBm
-            final_results.append(i)
-        #print 'Value list is %s long' % len(final_results)    
+        for dx, i in enumerate(separated):
+            if not (len(i) == 0) or i.startswith("$"):   
+                try: 
+                    i = (int(i, 16)/2)*-1#convert to dBm
+                except:
+                    #just take a guess here, hopefully something's not right in this area...
+                    prev = separated[dx-1]
+                    if (len(prev) == 0):
+                        prev = separated[dx-2]
+                    i = (int(prev[0:2],16)/2)*-1
+                final_results.append(i)
+            #print 'Value list is %s long' % len(final_results)    
         return final_results
             
     def compile_dictionary(self, values):
         freq_dict = {}
+        if len(values) != len(self.freq_list):
+            last = values[-1]
+            difference = len(self.freq_list) - len(values)
+            if difference > 0:
+                for i in range(difference):
+                    value = last
+                    values.append(value)
         for indx, freq in enumerate(self.freq_list):
             freq_dict[freq] = values[indx]
         return freq_dict
@@ -263,6 +277,7 @@ class RFExplorer:
         sorts the results dictionary and writes the file.
         Args: 
             filename: the name of the file that will be saved to the working directory
+            freq_dict: a dictionary of freq in kHz, dBm value pairs
         Returns: 
             file: CSV file with filename of filename arg formatted correctly
         Raises:
