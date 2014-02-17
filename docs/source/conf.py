1aebaa85b801c8e232476e2a5e3db1a462b040d1
matt dale <mattdalesmail@gmail.com>
2014-02-17 08:16:15 -0500

    changed the doc rel path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append('../../rf-explorer-python-api')
+sys.path.append('../../../rf-explorer-python-api')
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
